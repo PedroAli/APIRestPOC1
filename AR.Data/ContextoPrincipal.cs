2fc7b24d0efbd6c0412ab973ecba3a1637c56930
pedro ali <pedroali78@yahoo.com>
2021-10-06 15:40:15 -0300

    Teste

--- AR.Data/ContextoPrincipal.cs
+++ AR.Data/ContextoPrincipal.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AR.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace AR.Data
 {
-    public class ContextoPrincipal : DbContext
+    public class ContextoPrincipal : DbContext //framework Teste
     {
+        public ContextoPrincipal(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
     }
 }
